d1a6d5b45dec1fbb69aabca8d3bf0d44c922dba6
minerva <minerva.suvanto@gmail.com>
2020-12-01 14:26:26 +0100

    dict comprehension works maybe

--- tasks/dict_comp2.py
+++ tasks/dict_comp2.py
@@ -5,12 +5,16 @@ def group_count(input_list):
     make this into a one line dict comprehension
     """
     
-    group_count = {}
-    for item in input_list:
+    group_count = { item: input_list.count(item) for item in input_list }
+    # group_count = {}
+    # for item in input_list:
 
-        if item not in group_count:
-            group_count[item] = 0
+    #     if item not in group_count:
+    #         group_count[item] = 0
         
-        group_count[item] += 1
+    #     group_count[item] += 1
 
     return group_count
+
+input_list = ["1", "2", "2", "3","3"]
+print(group_count(input_list))
