4a6dd884081ebf2d7dca6d07ade992a339339474
caesarhawi <caesarhawi@hotmail.com>
2020-12-09 14:19:25 +0100

    Parse_XML

--- tasks/parse_xml.py
+++ tasks/parse_xml.py
@@ -1,4 +1,3 @@
-
 """
 write a function called parse_xml() which loads and parses data/21728182.xml. The data contains NER labeled sentences.
 
@@ -21,3 +20,28 @@ ners = {
 
 NOTE! NER_TYPE is the label not the drug text/name
 """
+
+import xml.etree.ElementTree as ET
+
+
+def parse_xml():
+    tree = ET.parse('data/21728182.xml')
+    root = tree.getroot()
+
+    sentences = {}
+    ners = {}
+    for child in root:
+        sentences[child.attrib['id']] = child.attrib['text']
+        if len(list(child.iter('entity'))) > 0:
+            ners[child.attrib['id']] = []
+            for entity in child.iter('entity'):
+                c = entity.attrib['charOffset']
+                t = tuple(c.split(sep='-'))
+                ners[child.attrib['id']].append((t, entity.attrib['type']))
+    return(sentences, ners)
+
+
+sentences, ners = parse_xml()
+print(sentences, ners)
+
+
